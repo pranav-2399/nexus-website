113139022a24067ebdbc4a015074aa582821aaa8
Pranav Anandan <prakulnand@gmail.com>
2025-12-15 23:20:09 +0530

    changed gitignore

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,16 +1,13 @@
 import { EventDetailPage } from "@/components/event-detail-page";
 
 type EventPageProps = {
-  // 👇 notice the Promise<>
   params: Promise<{ slug: string }>;
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 export default async function EventPage({ params, searchParams }: EventPageProps) {
-  console.log("EventPage params bfr:", params);
   const { slug } = await params;
   const query = await searchParams;
   const id = typeof query.id === "string" ? query.id : "";
-  console.log("EventPage params:", slug, id);
   return <EventDetailPage eventId={id} eventSlug={slug} />;
 }
